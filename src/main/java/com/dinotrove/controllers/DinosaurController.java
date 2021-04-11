318d2327fd5b39c739f4be3ecdbd36ab50bc1b4e
Rakesh Patnaik <r_patnaik@apple.com>
2021-04-11 11:48:13 -0700

    add delete function to complete create read update delete

--- src/main/java/com/dinotrove/controllers/DinosaurController.java
+++ src/main/java/com/dinotrove/controllers/DinosaurController.java
@@ -9,10 +9,12 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,7 +25,6 @@ import com.dinotrove.entities.Dinosaur;
 import com.dinotrove.entities.DinosaurDetail;
 import com.dinotrove.repositories.DinosaurRepository;
 import com.dinotrove.utils.StringHelper;
-import org.apache.commons.collections4.IteratorUtils;
 
 @Controller
 @RequestMapping("/dinosaur")
@@ -87,48 +88,13 @@ public class DinosaurController {
         }
         dinosaurRepository.save(editDinosaur);
         return "redirect:/dinosaur/crud/listing?dinosaurId="+editDinosaur.getDinosaurId();
-    }   
+    }  
     
-    @GetMapping("/adddinosaur")
-    public String showAddDinosaurForm(Dinosaur dinosaur) {
-        return "add-dinosaur";
-    }
+    @DeleteMapping("/crud/listing/{dinosaurId}")
+    public ResponseEntity<?> deleteCrudListing(@PathVariable("dinosaurId")Long dinosaurId, Model model) {
+        dinosaurRepository.deleteById(dinosaurId);
+        return ResponseEntity.ok(null);
+    }  
     
-    @PostMapping("/adddinosaur")
-    public String addDinosaur(@Valid Dinosaur dinosaur, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "add-dinosaur";
-        }
-        
-        dinosaurRepository.save(dinosaur);
-        return "redirect:/main";
-    }
-    
-    @GetMapping("/edit/{id}")
-    public String showUpdateForm(@PathVariable("id") long id, Model model) {
-        Dinosaur dinosaur = dinosaurRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid dinosaur Id:" + id));
-        model.addAttribute("dinosaur", dinosaur);
-        
-        return "update-dinosaur";
-    }
-    
-    @PostMapping("/update/{id}")
-    public String updateDinosaur(@PathVariable("id") long id, @Valid Dinosaur dinosaur, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            dinosaur.setDinosaurId(id);
-            return "update-dinosaur";
-        }
-        
-        dinosaurRepository.save(dinosaur);
-
-        return "redirect:/main";
-    }
-    
-    @GetMapping("/delete/{id}")
-    public String deleteDinosaur(@PathVariable("id") long id, Model model) {
-        Dinosaur dinosaur = dinosaurRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid dinosaur Id:" + id));
-        dinosaurRepository.delete(dinosaur);
-        
-        return "redirect:/main";
-    }
+   
 }
--- src/main/java/com/dinotrove/repositories/DinosaurRepository.java
+++ src/main/java/com/dinotrove/repositories/DinosaurRepository.java
@@ -2,10 +2,14 @@ package com.dinotrove.repositories;
 
 import java.util.List;
 
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dinotrove.entities.Dinosaur;
 
@@ -14,8 +18,32 @@ public interface DinosaurRepository extends CrudRepository<Dinosaur, Long> {
     
     List<Dinosaur> findByName(String name);
     
-    @Query(value = "select t.* from dinosaurs t where LOWER(t.name) LIKE :searchString OR LOWER(t.description) LIKE :searchString", nativeQuery = true)
+    @Query(value = "select t.* from dinosaurs t where LOWER(t.name) LIKE :searchString OR LOWER(t.description) LIKE :searchString", 
+    				nativeQuery = true)
     List<Dinosaur> findBySearchString(@Param("searchString")String searchString);
     
+    @Transactional  // START A Transaction to be atomic
+    @Modifying(clearAutomatically = true, flushAutomatically = true) // Ensure JPA context is clean and it refreshes after this delete
+    @Query(value = "update"
+    		+ "        dinosaurs"
+    		+ "    set"
+    		+ "        all_facts_document_id=:allFactsDocumentId,"
+    		+ "        description=:description,"
+    		+ "        dinosaur_type=:dinosaurType,"
+    		+ "        name=:name,"
+    		+ "        size_height=:sizeHeight,"
+    		+ "        size_length=:sizeLength,"
+    		+ "        size_weight=:sizeWeight"
+    		+ "    where"
+    		+ "        dinosaur_id=:dinosaurId",
+            nativeQuery = true)
+    void updateDinosaur(@Param("dinosaurId") Long dinosaurId, 
+    					@Param("name") String name, 
+    					@Param("dinosaurType") String dinosaurType,
+    					@Param("sizeHeight") Double sizeHeight, 
+    					@Param("sizeWeight") Double sizeWeight, 
+    					@Param("sizeLength") Double sizeLength, 
+    					@Param("description") String description, 
+    					@Param("allFactsDocumentId") String allFactsDocumentId);
     
 }
